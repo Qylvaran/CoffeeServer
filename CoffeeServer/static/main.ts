e16663500ce87873676343cf40ca3fa3c28559f1
Joseph Nichols <joseph.a.nichols@gmail.com>
2015-08-26 16:00:03 -0700

    Added main.ts

---
+++ CoffeeServer/static/main.ts
@@ -0,0 +1,13 @@
+﻿
+
+$(function () {
+    $.getJSON('month', function (monthList) {
+        $.each(monthList, function (index, mon) {
+            $('#month select').append($('<option></option>').val(mon).html(mon));
+        });
+        setMonth($('#month select').val());
+    });
+    $('select').change(function () {
+        setMonth($(this).val());
+    })
+});

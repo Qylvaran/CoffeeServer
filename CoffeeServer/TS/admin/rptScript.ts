45ed09c707d8affa4436b9b5fec2fc65e00fe2fd
Qylvaran <joseph.a.nichols@gmail.com>
2015-11-25 23:26:54 -0800

    Admin page exists, and has a big table with all the information in the database.

---
+++ CoffeeServer/TS/admin/rptScript.ts
@@ -0,0 +1,26 @@
+function fillTable(){
+  var tablef = document.createElement('tbody');
+  $.getJSON('month/', function (monthList) {
+    $.each(monthList, function (index, mon) {
+      $.getJSON('month/' + mon, function (mdata) {
+        for(var i:number = 0; i < mdata.length; i++) {
+          var row = document.createElement('tr');
+          $(row).append('<td>' + mon + ' ' + mdata[i].date + '</td>');
+          $(row).append('<td>' + mdata[i].volunteers[0].name + '</td>');
+          $(row).append('<td>' + mdata[i].volunteers[0].phone + '</td>');
+          $(row).append('<td>' + mdata[i].volunteers[0].email + '</td>');
+          $(row).append('<td>' + mdata[i].volunteers[1].name + '</td>');
+          $(row).append('<td>' + mdata[i].volunteers[1].phone + '</td>');
+          $(row).append('<td>' + mdata[i].volunteers[1].email + '</td>');
+          $(tablef).append(row);
+          console.log(row);
+        }
+      });
+    });
+  });
+  console.log(tablef);
+  $('#bigtable').append(tablef);
+}
+$(function(){
+  fillTable();
+})
--- CoffeeServer/gulpfile.js
+++ CoffeeServer/gulpfile.js
@@ -3,7 +3,17 @@ var ts = require('gulp-typescript');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
-var tsProject = ts.createProject('TS/tsconfig.json');
+var tsProject = ts.createProject('TS/main/tsconfig.json');
+var adminProject = ts.createProject('TS/admin/tsconfig.json');
+
+gulp.task('admin', function(){
+  var tsResult = adminProject.src()
+    .pipe(ts(adminProject))
+    .pipe(gulp.dest('./TS/JS/'));
+  return tsResult.pipe(browserify())
+    .pipe(rename('clientadmin.js'))
+    .pipe(gulp.dest('./static/clientscript/'));
+})
 
 gulp.task('default', function(){
   //code goes here.
